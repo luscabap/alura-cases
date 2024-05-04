16abb963493059e4f3a1d13099293ab2fc6123d8
luscabap <lucasbaptistasilva.dev@gmail.com>
2024-05-04 16:30:08 -0300

    First commit

---
+++ pages/faq.js
@@ -0,0 +1,10 @@
+import Link from "../src/components/Link";
+
+export default function FaqPage() {
+    return (
+        <div>
+            <h1>Página FAQ</h1>
+            <Link href="/">Voltar a Home Page</Link>
+        </div>
+    )
+}
