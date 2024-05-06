eec91a3bb135cba515388f548f400bf17904e668
luscabap <lucasbaptistasilva.dev@gmail.com>
2024-05-06 20:20:26 -0300

    Fetch API using getStaticProps

--- pages/faq.js
+++ pages/faq.js
@@ -1,10 +1,44 @@
 import Link from "../src/components/Link";
 
-export default function FaqPage() {
-    return (
-        <div>
-            <h1>Página FAQ</h1>
-            <Link href="/">Voltar a Home Page</Link>
-        </div>
-    )
+// SSG - Static Site Generation
+// SSR - Server Side Rendering
+// ISG - Incremental Static Generation
+
+// export async function getServerSideProps() {
+//     Em modo DEV, sempre roda a cada acesso.
+//     Roda a cada acesso que recebe.
+// }
+
+export async function getStaticProps() {
+  const URL_API = "https://gist.githubusercontent.com/omariosouto/0ceab54bdd8182cbd1a4549d32945c1a/raw/578ad1e8e5296fa048e3e7ff6b317f7497b31ad9/alura-cases-faq.json";
+  const response = await fetch(URL_API);
+  const data = await response.json();
+//Em modo DEV, sempre roda a cada acesso.
+//Roda somente no Build Time.
+
+  return {
+    props: {
+      data
+    },
+  };
+}
+
+export default function FaqPage({ data }) {
+
+  return (
+    <div>
+      <h1>Página FAQ</h1>
+      <Link href="/">Voltar a Home Page</Link>
+      <ul>
+        {data.map(({ answer, question }, i) => (
+          <li key={i}>
+            <article>
+              <h2>{question}</h2>
+              <p>{answer}</p>
+            </article>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
