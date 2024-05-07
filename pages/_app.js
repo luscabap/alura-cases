b3734ec093719e4926abb1e9c32775a210a9ec93
luscabap <lucasbaptistasilva.dev@gmail.com>
2024-05-07 10:41:57 -0300

    Google Analytics

--- pages/_app.js
+++ pages/_app.js
@@ -5,9 +5,23 @@ function MyApp({ Component, pageProps}){
     return (
         <>
             <Head>
-            <link rel="preconnect" href="https://fonts.googleapis.com" />
-            <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-            <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap" rel="stylesheet" />
+             {/*Google tag (gtag.js)*/}
+            <script async src="https://www.googletagmanager.com/gtag/js?id=G-9WWG208GLZ"></script>
+            <script 
+                dangerouslySetInnerHTML={{
+                    __html: `
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+
+                    gtag('config', 'G-9WWG208GLZ');
+                    `
+                }}
+            />
+
+                <link rel="preconnect" href="https://fonts.googleapis.com" />
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+                <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap" rel="stylesheet" />
             </Head>
             <GlobalStyle />
             <Component {...pageProps}/>
